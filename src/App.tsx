c1a3913c85a616dae85fd848234ebf9c1fe11fd2
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-18 23:02:16 -0400

    Used Flatlist instead of flex wrap to display currency buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { Image, SafeAreaView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react';
-import { currencyByCad } from './constants';
+import {
+  FlatList,
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Touchable,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+import {currencyByCad} from './constants';
 import Snackbar from 'react-native-snackbar';
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(+'');
+  const [exchangeValue, setExchangeValue] = useState(+'');
+  const [resultValue, setResultValue] = useState(+'');
 
-  const[inputValue,setInputValue] = useState(+'');
-  const[exchangeValue,setExchangeValue] = useState(+'');
-  const[resultValue,setResultValue] = useState(+'');
-
-
-  const convert = (value:number,currency) => {
-    
+  const convert = (value: number, Currency) => {
     if (!inputValue || isNaN(inputValue)) {
       return Snackbar.show({
         text: 'Please enter a valid CAD value!',
@@ -19,107 +30,119 @@ export default function App() {
         textColor: 'white',
         duration: Snackbar.LENGTH_LONG, // Default is LENGTH_SHORT
       });
-      
-      
     } else {
       const result = value * +inputValue;
       setResultValue(+result);
-      setExchangeValue(currency);
+      setExchangeValue(Currency);
     }
-    
-  }
-
+  };
 
   return (
     <SafeAreaView style={styles.appContainer}>
-      <View style={styles.headingTxtContainer}>
-        <Image style={styles.headingIcon} source={{ uri: 'https://cdn-icons-png.flaticon.com/128/6204/6204216.png' }}/>
-        <Text style={styles.headingTxt}> Currency Converter</Text>
-      </View>
-      <View style={styles.textInputContainer}>
-        <Text style={styles.text}>Enter CAD</Text>
-        <TextInput style={styles.textInput} onChangeText={(text) => setInputValue(+text)} placeholder='Ex:10'/>
-      </View>
-      <View style={styles.buttonContainer}>
-        {currencyByCad.map((currency) => (
-          <TouchableOpacity key={currency.symbol} style={styles.button} onPress={() => convert(currency.value, currency.symbol)}>
-            <Image style={styles.buttonImg} source={{ uri: currency.flag }} />
-            <Text style={styles.buttonTxt}>
-               {currency.name}
-            </Text>
-          </TouchableOpacity>
-      ))}
-        
-        
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={{flex: 1}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <View style={styles.headingTxtContainer}>
+              <Image
+                style={styles.headingIcon}
+                source={{
+                  uri: 'https://cdn-icons-png.flaticon.com/128/6204/6204216.png',
+                }}
+              />
+              <Text style={styles.headingTxt}> Currency Converter</Text>
+            </View>
+            <View style={styles.textInputContainer}>
+              <Text style={styles.text}>Enter CAD</Text>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={text => setInputValue(+text)}
+                placeholder="Ex:10"
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <FlatList
+                numColumns={3}
+                data={currencyByCad}
+                keyExtractor={item => item.symbol}
+                renderItem={({item}) => (
+                  <TouchableOpacity
+                    key={item.symbol}
+                    style={styles.button}
+                    onPress={() => convert(item.value, item.symbol)}>
+                    <Image
+                      style={styles.buttonImg}
+                      source={{uri: item.flag.toString()}}
+                    />
+                    <Text style={styles.buttonTxt}>{item.name}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </View>
 
-      </View>
-      <View style={styles.displayContainer}>
-        <Text style={styles.displayText}>{exchangeValue} {parseFloat(resultValue.toFixed(4))} </Text>
-      </View>
-      
-
+            <View style={styles.displayContainer}>
+              <Text style={styles.displayText}>
+                {exchangeValue} {parseFloat(resultValue.toFixed(4))}{' '}
+              </Text>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
-    flex:1,
+  appContainer: {
+    flex: 1,
     backgroundColor: '#38182F',
-    flexDirection: 'column'
-
+    flexDirection: 'column',
   },
   headingTxtContainer: {
     height: 100,
-    width: "100%",
+    width: '100%',
     borderColor: 'black',
     borderWidth: 5,
-    flexDirection:'row',
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black'
+    backgroundColor: 'black',
   },
-  headingTxt:{
+  headingTxt: {
     fontSize: 30,
     fontWeight: 'bold',
     color: '#330036',
     fontFamily: 'monospace',
   },
-  headingIcon:{
-    height:40,
-    width:30
-
+  headingIcon: {
+    height: 40,
+    width: 30,
   },
-  textInputContainer:{
-    
+  textInputContainer: {
     marginTop: 100,
     justifyContent: 'center',
-    alignItems: 'center'
-
+    alignItems: 'center',
   },
-  text:{
+  text: {
     fontSize: 30,
     marginBottom: 40,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-  textInput:{
+  textInput: {
     height: 50,
     width: 120,
     borderRadius: 15,
     backgroundColor: 'white',
-
-
   },
-  buttonContainer : {
+  buttonContainer: {
     margin: 30,
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'space-between'
-
-
+    justifyContent: 'space-between',
   },
-  button : {
+  button: {
     height: 40,
     width: 110,
     backgroundColor: 'black',
@@ -127,23 +150,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 15,
-    flexDirection: 'row'
-    
-    
-    
+    flexDirection: 'row',
   },
-  buttonTxt : {
+  buttonTxt: {
     color: '#56666B',
     fontSize: 17,
-    
   },
-  buttonImg:{
-    margin:5,
+  buttonImg: {
+    margin: 5,
     height: 20,
-    width:20
-
+    width: 20,
   },
-  displayContainer:{
+  displayContainer: {
     width: '60%',
     height: 100,
     backgroundColor: '#EEE1B3',
@@ -156,8 +174,7 @@ const styles = StyleSheet.create({
     borderBlockColor: 'black',
     borderWidth: 10,
   },
-  displayText : {
+  displayText: {
     fontSize: 25,
-
-  }
-})
+  },
+});
